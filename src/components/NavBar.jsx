cc17fc1882598b720f5347eb836c96f96c736709
bangsiren <bangsir61@gmail.com>
2022-09-10 11:55:20 -1100

    first-commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function NavBar() {
+    return (
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+        </nav>
+    )
+}
