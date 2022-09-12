78a79c1baa0b4e978ea7b8cc3ad0b0ee794bbea8
bangsiren <bangsir61@gmail.com>
2022-09-11 16:23:41 -1100

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import Counters from './components/Counters';
+
 function App() {
+
+let [counters, setCounters ] = useState([
+
+    {id:1, value: 4},
+    {id:2, value: 0},
+    {id:3, value: 0},
+    {id:4, value: 0},
+    
+])
+
+function handleIncrement(counter){
+    console.log(counter)
+    const incrementCounter = [...counters]
+
+    const index = incrementCounter.indexOf(counter)
+    incrementCounter[index] = { ...counter }
+    incrementCounter[index].value++
+    setCounters(incrementCounter)
+} 
+
+function handleDelete(counterID) {
+
+    console.log(counterID)
+    const newCounter = counters.filter( c => c.id !== counterID)
+    setCounters(newCounter);
+}
+
+function handleReset() {
+    const resetCounter = counters.map(c => {
+        c.value = 0;
+        return c;
+    })
+    setCounters(resetCounter)
+}
+
   return (
      <React.Fragment>
        <NavBar/>
+
        <main className='container'>
-          <Counters/>
+          <Counters
+          counters={counters}
+          onReset={handleReset}
+          onIncrement={handleIncrement}
+          onDelete={handleDelete}
+
+          />
        </main>
      </React.Fragment>
   );
--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,43 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Counter from "./Counter";
-export default function Counters() {
+export default function Counters({ counters, onIncrement, onReset, onDelete }) {
 
-    let [counters, setCounters ] = useState([
-
-        {id:1, value: 4},
-        {id:2, value: 0},
-        {id:3, value: 0},
-        {id:4, value: 0},
-    ])
-    function handleIncrement(counter){
-        console.log(counter)
-        const incrementCounter = [...counters]
-        const index = incrementCounter.indexOf(counter)
-        incrementCounter[index] = { ...counter }
-        incrementCounter[index].value++
-        setCounters(incrementCounter)
-    } 
-    function handleDelete(counterID) {
-   
-        console.log(counterID)
-        const newCounter = counters.filter( c => c.id !== counterID)
-        setCounters(newCounter);
-    }
-    
-    function handleReset() {
-        const resetCounter = counters.map(c => {
-            c.value = 0;
-            return c;
-        })
-        setCounters(resetCounter)
-    }
     return (
         <div>
-            <button onClick={handleReset} className="btn btn-primary btn-sm m-2">reset</button>
-           {counters.map(counter=> <Counter  
-           onIncrement = {handleIncrement}
-           onDelete={handleDelete} key={counter.id}
-            counter = {counter} id={counter.id}
+            <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+           { counters.map(counter=> <Counter  
+             onIncrement = {onIncrement}
+             onDelete={onDelete} key={counter.id}
+             counter = {counter} id={counter.id}
             />)}
         </div>
     )
